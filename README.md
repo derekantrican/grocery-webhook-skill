d4ad2a57b3c51cb6edfcf39a2b593499be22d224
Derek Antrican <derekantrican@gmail.com>
2022-08-23 09:34:52 -0700

    Updated content from previous repo

--- README.md
+++ README.md
@@ -1,22 +1,23 @@
-# <img src="https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/robot.svg" card_color="#22A7F0" width="50" height="50" style="vertical-align:bottom"/> Grocery Webhook
+# Grocery 2 Webhook
 Add items to a grocery list via a webhook
 
-## About
-Capture commands to add items to a grocery list and call your own webhook. by creating your own webhook, you can customize what service (eg todoist, any.do, email, etc) to act upon with the results!
+## About 
+Capture commands to add items to a grocery list and call your own webhook. By creating your own webhook, you can customize what service (eg Todoist, Any.Do, email, etc) to act upon with the results!
 
-## Examples
-* "Add {item} to my grocery list"
-* "Add {item} to my shopping list"
+To configure this skill go [to the skill wiki]()
 
-## Credits
-Derek Antrican
+## Examples 
+* "Add _______ to my grocery list"
+* "Add _______ to my shopping list"
+
+## Credits 
+Derek Antrican (@derekantrican)
 
 ## Category
 **Productivity**
 
 ## Tags
-#Grocery
-#Shopping
-#List
-#Webhook
-
+#grocery
+#shopping
+#list
+#webhook
--- __init__.py
+++ __init__.py
@@ -1,19 +1,47 @@
+# Copyright 2022 Derek Antrican
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 from mycroft import MycroftSkill, intent_file_handler
+import requests
 
-
-class GroceryWebhook(MycroftSkill):
+class GroceryWebhookSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+    
+    def initialize(self):
+        self.settings_change_callback = self.on_settings_changed
+        self.on_settings_changed()
 
-    @intent_file_handler('webhook.grocery.intent')
-    def handle_webhook_grocery(self, message):
-        item = message.data.get('item')
+    def on_settings_changed(self):
+        self.webhook_url = self.settings.get('webhook_url')
+        self.http_method = self.settings.get('http_method')
 
-        self.speak_dialog('webhook.grocery', data={
-            'item': item
-        })
+    @intent_file_handler('add.intent')
+    def enquire_new_email(self, message):
+        item = message.data.get('item').lower()
+        
+        # Todo: validate webhook_url & http_method by checking "" or None
+
+        if self.http_method == "post":
+            requests.post(self.webhook_url, data = item)
+        elif self.http_method == "get":
+            requests.get(self.webhook_url, data = item)
+
+        # Todo: report any errors
+        self.speak_dialog("added", data={"item": item})
 
 
 def create_skill():
-    return GroceryWebhook()
-
+    return GroceryWebhookSkill()
